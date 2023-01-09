a86c6233a044896dda4e93c2ad8e463dd9df30b2
Nazar <nazar.demianyk-ki211@gmail.com>
2023-01-09 20:54:41 +0200

    Added i18n!

--- adw/src/main/java/my/advanced/project/adw/service/MagazineService.java
+++ adw/src/main/java/my/advanced/project/adw/service/MagazineService.java
@@ -2,6 +2,8 @@ package my.advanced.project.adw.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,18 +11,24 @@ import my.advanced.project.adw.dao.MagazineRepository;
 import my.advanced.project.adw.domain.Magazine;
 @Service
 public class MagazineService {
+	
+	private Logger logger = LoggerFactory.getLogger(MagazineService.class);
+	
 	@Autowired
 	private MagazineRepository magazineRepository;
 	
 	public Magazine save(Magazine magazine) {
+		logger.info("Create magazine item {} : " + magazine);
 		return magazineRepository.save(magazine);
 	}
 	
 	public List<Magazine> getAllMagazines(){
+		logger.info("Get all magazines items");
 		return magazineRepository.findAll();
 	}
 	
 	public Magazine findById(Integer id) {
+		logger.info("Get magazine item by id: " + id);
 		return magazineRepository.findById(id).get();
 	}
 }
